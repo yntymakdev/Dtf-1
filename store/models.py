d1ad3ad6931033457d04a4bc83252b31094a67a7
yntymakdev <kubanychevyntymak21@gmail.com>
2025-03-31 13:11:47 +0600

    i ll do my best

--- store/models.py
+++ store/models.py
@@ -8,5 +8,12 @@ class Product(models.Model):
     description  =models.TextField()
     price = models.PositiveIntegerField(default=0)
     category = models.ForeignKey(Category,on_delete=models.CASCADE)
+    created_date = models.DateField(auto_now_add=True)
+    have = models.BooleanField(default=True)
 class Comment(models.Model):
     name = models.CharField(max_length=16)
+    text = models.TextField()
+    product = models.ForeignKey(Product,on_delete=models.CASCADE)
+    created_date =  models.DateTimeField(auto_now_add=True)
+
+
