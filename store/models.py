bbb6d069015ee6ef77b8a8faea0e7bcb503e6a67
yntymakdev <kubanychevyntymak21@gmail.com>
2025-03-31 13:07:04 +0600

    understoo

--- store/models.py
+++ store/models.py
@@ -1,6 +1,12 @@
 from django.db import models
+from django.db.models import CASCADE
 
+class Category(models.Model):
+    title  = models.CharField(max_length=16)
 class Product(models.Model):
     product_name = models.CharField(max_length=16)
     description  =models.TextField()
     price = models.PositiveIntegerField(default=0)
+    category = models.ForeignKey(Category,on_delete=models.CASCADE)
+class Comment(models.Model):
+    name = models.CharField(max_length=16)
