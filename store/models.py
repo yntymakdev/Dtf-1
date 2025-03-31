ac731353cb8538758345845aa9eb476afb09e234
yntymakdev <kubanychevyntymak21@gmail.com>
2025-03-31 21:08:17 +0600

    as

--- store/models.py
+++ store/models.py
@@ -3,6 +3,10 @@ from django.db.models import CASCADE
 
 class Category(models.Model):
     title  = models.CharField(max_length=16)
+
+    def __str__(self):
+        return  self.title
+
 class Product(models.Model):
     product_name = models.CharField(max_length=16)
     description  =models.TextField()
@@ -10,10 +14,14 @@ class Product(models.Model):
     category = models.ForeignKey(Category,on_delete=models.CASCADE)
     created_date = models.DateField(auto_now_add=True)
     have = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.product_name
 class Comment(models.Model):
     name = models.CharField(max_length=16)
     text = models.TextField()
     product = models.ForeignKey(Product,on_delete=models.CASCADE)
     created_date =  models.DateTimeField(auto_now_add=True)
-
+    def __str__(self):
+        return f'{self.name } - {self.product}'
 
