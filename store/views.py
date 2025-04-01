b0037fb0b4d99c26ecfb6260b663cb417df29ff6
yntymakdev <kubanychevyntymak21@gmail.com>
2025-04-01 15:07:48 +0600

    costumuzed

--- store/views.py
+++ store/views.py
@@ -1,7 +1,5 @@
 from django.views.generic import ListView
-
 from .models import Product
-
 class ProductListView(ListView):
     queryset = Product.objects.all()
     template_name = 'product_list.html'
